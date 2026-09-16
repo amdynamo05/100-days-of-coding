0f1a9683a13c76818af8d158357886e3cc56d7e5
amdynamo05 <amdynamo05@gmail.com>
2026-09-16 21:17:02 +0530

    Create Q10.c

---
+++ Day5/Q10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int total,hours,minutes,seconds;
+printf("Enter time in seconds: ");
+scanf("%d",&total);
+hours=total/3600;
+minutes=(total%3600)/60;
+seconds=total%60;
+printf("Time= %dhr : %dmin : %dsec ",hours,minutes,seconds);
+return 0;
+}
